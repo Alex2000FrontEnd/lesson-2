3d56d691a41961e7bd6adc90302d30defa0d22e1
Alex <salobuta.alex@gmail.com>
2022-12-10 08:52:09 +0700

    Первый урок закончен

--- js/script.js
+++ js/script.js
@@ -14,13 +14,47 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    removeAdv();
+    drama();
+    imgBg();
+    moviesDB();
+
+    function moviesDB() {
+        const list = document.querySelector('.promo__interactive-list');
+        movieDB.movies.sort();
+        list.innerHTML = '';
+        movieDB.movies.forEach((item, i) => {
+            list.innerHTML += `<li class="promo__interactive-item">${i + 1} ${movieDB.movies[i]}
+                <div class="delete"></div>
+            </li>`;
+        });
+
+    }
+
+    function imgBg() {
+        document.querySelector('.promo__bg').style.backgroundImage = 'url(../img/bg.jpg)'
+    }
+
+    function drama() {
+        document.querySelector('.promo__genre').textContent = 'драма';
+    }
+
+    function removeAdv() {
+        const adv = document.querySelectorAll('.promo__adv img');
+        adv.forEach(item => {
+            item.remove();
+        });
+    }
+});
 
